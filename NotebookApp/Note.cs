50d60d32f76b32a9664cdc1b5a239aede0a781d2
Unknown <anna-maks99@lenta.ru>
2019-11-06 23:48:13 +0300

    lab1

---
+++ NotebookApp/Note.cs
@@ -0,0 +1,178 @@
+﻿using System;
+
+namespace NotebookApp
+{
+    class Note
+    {
+        private static int _numberOfNotes;
+        private string _name;
+        private string _surname;
+        private string _lastname;
+        private ulong _phoneNumber;
+        private string _country;
+        private DateTime _birthday;
+        private string _organization;
+        private string _post;
+        private string _other;
+        private int _id;
+
+        public string Id
+        {
+            get => _id.ToString();
+            set => _id = int.Parse(value);
+        } 
+
+        public string Surname
+        {
+            get => _surname;
+            set
+            {
+                while (true)
+                {
+                    
+                    Notebook.CheckNecessaryField(value, out bool isCorrect);
+                    if (isCorrect)
+                    {
+                        _surname = value;
+                        break;
+                    }
+                    value = Console.ReadLine();
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                while (true)
+                {
+                    Notebook.CheckNecessaryField(value, out bool isCorrect);
+                    if (isCorrect)
+                    {
+                        _name = value;
+                        break;
+                    }
+                    value = Console.ReadLine();
+                }
+            }
+        }
+
+        public string Lastname
+        {
+            get => _lastname;
+            set
+            {
+                Notebook.CheckUnnecessaryField(ref value);
+                _lastname = value;
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get => _phoneNumber.ToString();
+            set
+            {
+                while (true)
+                {
+                    bool isUniq = true;
+                    bool isCorrect = ulong.TryParse(value, out ulong phoneNumber);
+                    if (!isCorrect) 
+                    {
+                        Console.WriteLine("Номер введен некорректно. Номер должен содержать только цифры.");
+                        value = Console.ReadLine();
+                        continue;
+                    }
+
+                    foreach (var item in Notebook.listNotes)
+                    {
+                        if (ulong.Parse(item.PhoneNumber) == phoneNumber)
+                        {
+                            if (phoneNumber == _phoneNumber)
+                            {
+                                break;
+                            }
+                            Console.WriteLine("Такой номер уже существует");
+                            isUniq = false;
+                            break;
+                        }
+                    }
+
+                    if (isUniq)
+                    {
+                        _phoneNumber = phoneNumber;
+                        break;
+                    }
+                    value = Console.ReadLine();
+                }
+            }
+        }
+
+        public string Country
+        {
+            get => _country;
+            set
+            {
+                while (true)
+                {
+                    Notebook.CheckNecessaryField(value, out bool isCorrect);
+                    if (isCorrect)
+                    {
+                        _country = value;
+                        break;
+                    }
+                    value = Console.ReadLine();
+                }
+            }
+        }
+
+        public string Birthday
+        {
+            get => _birthday.ToString();
+            set
+            {
+                if (!DateTime.TryParse(value, out _birthday))
+                {
+                    _birthday = new DateTime(1, 1, 1);
+                }
+            }
+        }
+
+        public string Organization
+        {
+            get => _organization;
+            set
+            {
+                Notebook.CheckUnnecessaryField(ref value);
+                _organization = value;
+            }
+        }
+
+        public string Post
+        {
+            get => _post;
+            set
+            {
+                Notebook.CheckUnnecessaryField(ref value);
+                _post = value;
+            }
+        }
+
+        public string Other
+        {
+            get => _other;
+            set
+            {
+                Notebook.CheckUnnecessaryField(ref value);
+                _other = value;
+            }
+        }
+
+        public Note()
+        {
+            Id = _numberOfNotes++.ToString();
+        }
+
+    }
+}
---
+++ NotebookApp/ProgramLogic.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace NotebookApp
+{
+    static class ProgramLogic
+    {
+        /// <summary>
+        /// Выводит приветствие на экран
+        /// </summary>
+        internal static void Greeting()
+        {
+            Console.WriteLine("Добро пожаловать в записную книжку!");
+        }
+
+        /// <summary>
+        /// Реализует основную логику программы
+        /// </summary>
+        internal static void Start()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Выберете дальнейшее действие:\n" +
+                                  "- Создать новую запись (Введите 1)\n" +
+                                  "- Редактировать существующую запись (Введите 2)\n" +
+                                  "- Удалить запись (Введите 3)\n" +
+                                  "- Просмотреть запись (Введите 4)\n" +
+                                  "- Просмотреть все записи (Введите 5)\n" +
+                                  "- Выйти из программы (Введите 6)");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.Clear();
+                        Notebook.CreateNewNote();
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Notebook.EditNote();
+                        break;
+                    case "3":
+                        Console.Clear();
+                        Notebook.DeleteNote();
+                        break;
+                    case "4":
+                        Console.Clear();
+                        Notebook.ReadNote();
+                        break;
+                    case "5":
+                        Console.Clear();
+                        Notebook.ShowAllNotes();
+                        break;
+                    case "6":
+                        Console.Clear();
+                        Console.WriteLine("Были рады вас видеть");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Неверно выбрана команда. Попробуйте еще раз");
+                        break;
+                }
+            }
+        } 
+    }
+}
